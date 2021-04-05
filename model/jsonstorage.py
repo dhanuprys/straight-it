0aab409f8976843f827c4548c7f3d751444898a0
dhanuprys <dhanuprys@gmail.com>
2021-04-05 21:46:08 +0800

    Create storage logic

---
+++ model/jsonstorage.py
@@ -0,0 +1,78 @@
+from os import path
+from model.basemodel import BaseModel
+import orjson
+
+class JsonStorage(BaseModel):
+    def __init__(self):
+        real_storage_path = path.join(
+            path.dirname(
+                path.realpath(__file__)
+            ), 
+            "db.json"
+        )
+        
+        if not path.isfile(real_storage_path):
+            with open(real_storage_path, "w+") as storage:
+                storage.write("{}")
+
+        self.real_storage_path = real_storage_path
+    def store(self, gateway, target):
+        collection = self.__read_storage()
+        collection_key_check = collection.get(gateway)
+        
+        if collection_key_check:
+            raise KeyError("Storage key is already present")
+
+        # FIXME: Add url validator
+        collection[gateway] = target
+
+        return self.__put_storage(collection)
+    def update(self, gateway, new_target):
+        collection = self.__read_storage()
+        collection_key_check = collection.get(gateway)
+
+        if not collection_key_check:
+            raise KeyError("Key is not avalaible")
+
+        # FIXME: Add URL validator
+        collection[gateway] = new_target
+
+        return self.__put_storage(collection)
+    def delete(self, gateway):
+        collection = self.__read_storage()
+        collection_key_check = collection.get(gateway)
+
+        if not collection_key_check:
+            raise KeyError("Key is not avalaible")
+
+        del collection[gateway]
+
+        return self.__put_storage(collection)
+    def get_gateway(self, target):
+        collection = self.__read_storage()
+        
+        for gateway in collection.keys():
+            if collection[gateway] == target:
+                return gateway
+
+        return None
+    def get_target(self, gateway):
+        collection = self.__read_storage()
+        return collection.get(gateway)
+    def __read_storage(self):
+        try:
+            with open(self.real_storage_path, 'r') as storage:
+                return orjson.loads(storage.read())
+        except:
+            return {}
+    def __put_storage(self, object_data):
+        try:
+            object_data = str(orjson.dumps(object_data), "utf-8")
+
+            with open(self.real_storage_path, 'w') as storage:
+                storage.write(object_data)
+        except:
+            return False
+        
+        return True
+
